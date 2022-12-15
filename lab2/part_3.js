6ea31cccee929d6710a6b0e538e9b0ab5f52d7f7
Alena313 <73710857+Alena313@users.noreply.github.com>
2022-12-15 08:26:36 +0300

    Add files via upload

---
+++ lab2/part_3.js
@@ -0,0 +1,8 @@
+    var y = prompt("введите первый катет");
+    var z = prompt("введите второй катет");
+    y=Number(y);
+    z=Number(z);
+    var x=Math.sqrt(Math.pow(y,2)+Math.pow(z,2));
+    var result = confirm(x+ "\n" + "Все верно?")
+   
+

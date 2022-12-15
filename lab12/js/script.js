ab90553647c686fcc5d7fc33dfdc8a97723375d6
Alena313 <73710857+Alena313@users.noreply.github.com>
2022-12-15 08:32:23 +0300

    Add files via upload

---
+++ lab12/js/script.js
@@ -0,0 +1,44 @@
+// first
+// $(document).ready(function(){
+//     var myLogo = $("img[alt='Хедер']");
+//     myLogo.hide(3000);
+//     myLogo.show(3000);
+// });
+
+// second
+// $(document).ready(function(){
+//     var myLogo = $("img:not(:first-child)");
+//     myLogo.hide(5000);
+// });
+
+// third
+// $(document).ready(function third(id, attribute, value) {
+  
+//     id = "div_for_img"
+//     attribute = "left"
+//     value = "150px"
+//     $("#" + id).css(attribute, value);
+// }
+// )
+
+
+// fourth
+// $(document).ready(function(){
+//     var myLogo = $("#img_2");
+//     myLogo.css("border", "1px solid #333333")
+//     .animate({
+//         "border-width":"5px"
+//     },5000, function(){
+//      $("img[alt='Хедер']").fadeOut(5000)})
+// });
+
+//fifth
+
+// $(document).ready(function () {
+//     let p = $("<p id='newparagraph'>Абзац, добавленный после картинки, с помощью  after()</p>");
+//     // $($("#main_div").after('<p>Абзац, добавленный после картинки, с помощью  after()')).attr('id','newparagraph');
+//     $("#main_div").after(p);
+// //    let iii=$(#ii);
+//     p.css("background", "black")
+//         .css("color", "white")
+// });

31e0aba41909a33349fd4291cb5a6ef80312f7f9
Alena313 <73710857+Alena313@users.noreply.github.com>
2022-12-15 08:30:23 +0300

    Add files via upload

---
+++ lab9/lab9.js
@@ -0,0 +1,27 @@
+function Show(n) {
+    n.style.visibility = "visible"    
+}
+function Hide(n) {
+    n.style.visibility = "hidden"
+}
+function left_on_50() {
+    myPict.style.position="relative";
+  myPict.style.left = (myPict.offsetLeft + 50) + "px";
+
+}
+function left_on_20() {
+    myPict.style.position="relative";
+  myPict.style.left = (myPict.offsetLeft + 20) + "px"
+}
+function left_on_10() {
+    myPict.style.position="relative";
+  myPict.style.left = (myPict.offsetLeft + 10) + "px"
+}
+function right_on_20() {
+    myPict.style.position="relative";
+  myPict.style.left = (myPict.offsetLeft - 20) + "px"
+}
+function right_on_50() {
+    myPict.style.position="relative";
+  myPict.style.left = (myPict.offsetLeft - 50) + "px"
+}
